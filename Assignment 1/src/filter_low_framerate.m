82bf4238dabdcfc574041df5a62d6a27b60c13a2
Matej Bartalsky <matej.bartalsky@gmail.com>
2017-11-05 13:03:56 +0100

    Add improvement to low_framerate_function to avoid exceptions

--- Assignment 1/src/filter_low_framerate.m
+++ Assignment 1/src/filter_low_framerate.m
@@ -1,15 +1,15 @@
 % FILTER_LOW_FRAMERATE(VIDEO, SOURCE_FRAMERATE, TARGET_FRAMERATE) is a time based
 % filter which changes the framerate from SOURCE_FRAMERATE to TARGET_FRAMERATE.
-%  
+%
 %   VIDEO:  a structure containing an array of frames where frame(1)
 %   contains the most current frame.
 %
 %   VIDEO = FILTER_LOW_FRAMERATE(VIDEO, SOURCE_FRAMERATE, TARGET_FRAMERATE)
-%   returns the original video structure with the updated current video.frame(1).filtered. 
-% 
+%   returns the original video structure with the updated current video.frame(1).filtered.
+%
 % Example:
 %   VIDEO = FILTER_LOW_FRAMERATE(VIDEO, 25, 19) returns in total 25 frames per second
-%   but only 19 different ones. 6 frames are randomly replaced by their forerunners. 
+%   but only 19 different ones. 6 frames are randomly replaced by their forerunners.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -25,53 +25,39 @@
 %       double_frame array to determine wheter to replace or not.
 %   PHYSICAL BACKGROUND:
 %       To save money early films were filmed by hand cranked cameras
-%       in ~14fps As each frame was a picture on a tape roll it was kept as 
-%       low as possible so the movement is still perceived fluently. 
-%       
+%       in ~14fps As each frame was a picture on a tape roll it was kept as
+%       low as possible so the movement is still perceived fluently.
+%
 function video = filter_low_framerate(video, source_fps, target_fps)
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Check if we process the first frame and init filter 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    if (~isfield(video, 'filter_low_framerate'))
-        video.filter_low_framerate.start_frame = video.start_frame;
-        video.filter_low_framerate.double_frame = zeros(1, source_fps);
-        skip_num = (source_fps - target_fps);
-        rand = randperm(25, skip_num);
-        video.filter_low_framerate.double_frame = zeros(1, source_fps);
-        for a = 1: skip_num
-            video.filter_low_framerate.double_frame(rand(a))=1;
-        end
-        %{
-        skip = source_fps / skip_num;
-        if skip >=2
-            skip_index = round(skip);
-            video.filter_low_framerate.double_frame = zeros(1, source_fps);
-            video.filter_low_framerate.double_frame(skip_index : skip_index : skip_index*skip_num) = 1;
-            video.filter_low_framerate.double_frame=video.filter_low_framerate.double_frame(1,1 : source_fps);
-        else
-            keep_index = round(source_fps / target_fps);
-            video.filter_low_framerate.double_frame = ones(1, source_fps);
-            video.filter_low_framerate.double_frame(1 : keep_index : (keep_index * target_fps)-1) = 0;
-            video.filter_low_framerate.double_frame=video.filter_low_framerate.double_frame(1,1 : source_fps);
-        end
-        frames= sum(video.filter_low_framerate.double_frame);
-        target= frames + target_fps;
-        %}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Check if we process the first frame and init filter
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if (~isfield(video, 'filter_low_framerate'))
+    video.filter_low_framerate.start_frame = video.start_frame;
+    video.filter_low_framerate.double_frame = zeros(1, source_fps);
+    skip_num = (source_fps - target_fps);
+    ra = randperm(25, skip_num);
+    
+    % The very first frame cannot be replaced by its predecessor,
+    % because it does not exist.
+    while video.frame(1).frame_nr == 1 && any(ra(:) == 1)
+        ra = randperm(25, skip_num);
     end
-    % Check if it's the very first frame, this cannot be replaced by a
-    % predecessor because it does not exist.
-    if video.frame(1).frame_nr ~= 1
-        % Check if the source_fps are already done.
-        frame_pos = mod(video.frame(1).frame_nr, source_fps);
-        if frame_pos ~= 0
-            if video.filter_low_framerate.double_frame(frame_pos) == 1
-            video.frame(1).filtered = video.frame(2).filtered;
-            end
-        else
-            % remove struct field, so for the next call, double_frame can be
-            % reinitialized.
-            video = rmfield(video,'filter_low_framerate');
-        end
+    
+    video.filter_low_framerate.double_frame = zeros(1, source_fps);
+    video.filter_low_framerate.double_frame(ra(1:skip_num))=1;
+end
+
+% Check if the source_fps are already done.
+frame_pos = mod(video.frame(1).frame_nr, source_fps);
+if frame_pos ~= 0
+    if video.filter_low_framerate.double_frame(frame_pos) == 1
+        video.frame(1).filtered = video.frame(2).filtered;
     end
+else
+    % remove struct field, so for the next call, double_frame can be
+    % reinitialized.
+    video = rmfield(video,'filter_low_framerate');
+end
 end
